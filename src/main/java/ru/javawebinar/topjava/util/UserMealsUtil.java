7908d128e84c539488e667c236536fc753784401
RegisSDA <regisakai@rambler.ru>
2017-07-13 12:31:42 +0300

    Prepare to HW0

---
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -0,0 +1,54 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.model.UserMealWithExceed;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.*;
+
+/**
+ * GKislin
+ * 31.05.2015.
+ */
+public class UserMealsUtil {
+    public static void main(String[] args) {
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+        );
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+
+// .toLocalDate();
+//        .toLocalTime();
+    }
+
+    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        Map<LocalDate,Integer> map = new HashMap<>();
+        for (UserMeal meal:mealList){
+            if (map.containsKey(meal.getDateTime().toLocalDate())){
+                map.put(meal.getDateTime().toLocalDate(),map.get(meal.getDateTime().toLocalDate())+meal.getCalories());
+            }else{
+                map.put(meal.getDateTime().toLocalDate(),meal.getCalories());
+            }
+        }
+        List<UserMealWithExceed> results = new ArrayList<>();
+        for (UserMeal meal:mealList){
+            if (TimeUtil.isBetween(meal.getDateTime().toLocalTime(),startTime,endTime)){
+                if (map.get(meal.getDateTime().toLocalDate())>caloriesPerDay) {
+                    results.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true));
+                }
+                else{
+                    results.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), false));
+                }
+            }
+        }
+        return results;
+    }
+}
